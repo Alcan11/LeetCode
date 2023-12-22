acdcf37f2375fe89530603568d511a0778b25d13
Álvaro Gómez Salguero <136193455+Alcan11@users.noreply.github.com>
2023-12-22 07:33:09 +0100

    Create solution.java

---
+++ Algorithms/14/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+  public String longestCommonPrefix(String[] strs) {
+    String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            while (j < prefix.length() && j < strs[i].length() && prefix.charAt(j) == strs[i].charAt(j)) {
+                j++;
+            }
+            prefix = prefix.substring(0, j);
+        }
+        return prefix;
+    }
+}
