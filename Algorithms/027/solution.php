03ea5074624d74641d3c57b29529e43ce3ef1c6b
Álvaro Gómez Salguero <136193455+Alcan11@users.noreply.github.com>
2024-01-19 07:30:09 +0100

    Create solution.php

---
+++ Algorithms/027/solution.php
@@ -0,0 +1,15 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $val
+     * @return Integer
+     */
+    function removeElement(&$nums, $val) {
+        foreach($nums as $key=>$num){
+            if($nums[$key] == $val){
+                unset($nums[$key]);
+            }
+        }
+    }
+}
